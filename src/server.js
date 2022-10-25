e280ae60e4b4036dd6d4eef2bfa350e5e9585ae8
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-10-25 18:04:32 +0900

    Today's Assignment

--- src/server.js
+++ src/server.js
@@ -5,14 +5,22 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("<h1>I still love you.</h1>");
+  return res.send("<h1>Hello my name is Yunha.</h1>");
+};
+const handleAbout = (req, res) => {
+  return res.send("<h2>I'm from korea.</h2>");
+};
+const handleContact = (req, res) => {
+  return res.send("<h3>My hobby is going to gym and codding.</h3>");
 };
 
 const handleLogin = (req, res) => {
-  return res.send({ message: "Login here." });
+  return res.send("</h4>I'will be front-end developer.</h4>");
 };
 
 app.get("/", handleHome);
+app.get("/about", handleAbout);
+app.get("/contact", handleContact);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
