c49eb748408fa49e376b8fe14bd3bddf0e8407db
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-10-26 14:46:12 +0900

    Middlewares part Two

--- src/server.js
+++ src/server.js
@@ -4,26 +4,32 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("<h1>Hello my name is Yunha.</h1>");
-};
-const handleAbout = (req, res) => {
-  return res.send("<h2>I'm from korea.</h2>");
-};
-const handleContact = (req, res) => {
-  return res.send("<h3>My hobby is going to gym and codding.</h3>");
+  return res.send("I love middleware");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("</h4>I'will be front-end developer.</h4>");
-};
-
-app.get("/", handleHome);
-app.get("/about", handleAbout);
-app.get("/contact", handleContact);
-app.get("/login", handleLogin);
-
+app.get("/", gossipMiddleware, handleHome);
 const handleListening = () =>
   console.log(`Sever listening on port http://localhost:${PORT}🚀`);
 
 app.listen(PORT, handleListening);
+
+// const handleAbout = (req, res) => {
+//   return res.send("<h2>I'm from korea.</h2>");
+// };
+// const handleContact = (req, res) => {
+//   return res.send("<h3>My hobby is going to gym and codding.</h3>");
+// };
+
+// const handleLogin = (req, res) => {
+//   return res.send("</h4>I'will be front-end developer.</h4>");
+// };
+
+// app.get("/about", handleAbout);
+// app.get("/contact", handleContact);
+// app.get("/login", handleLogin);
