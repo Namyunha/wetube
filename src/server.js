f818c723e67e6a665d4e16acd6afd5df3eabc5eb
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-10-26 15:50:11 +0900

    Severs Recap

--- src/server.js
+++ src/server.js
@@ -4,26 +4,7 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  next();
-  console.log("Allowed, you may continue.");
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middleware");
-};
-
-app.use(logger);
-app.use(privateMiddleware);
+const handleHome = () => console.log("home");
 
 app.get("/", handleHome);
 
@@ -31,18 +12,3 @@ const handleListening = () =>
   console.log(`Sever listening on port http://localhost:${PORT}🚀`);
 
 app.listen(PORT, handleListening);
-
-// const handleAbout = (req, res) => {
-//   return res.send("<h2>I'm from korea.</h2>");
-// };
-// const handleContact = (req, res) => {
-//   return res.send("<h3>My hobby is going to gym and codding.</h3>");
-// };
-
-// const handleLogin = (req, res) => {
-//   return res.send("</h4>I'will be front-end developer.</h4>");
-// };
-
-// app.get("/about", handleAbout);
-// app.get("/contact", handleContact);
-// app.get("/login", handleLogin);
