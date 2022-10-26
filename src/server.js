9ce8a45d4603c47cc497e4f24abbf3a2ae3fe1b7
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-10-26 16:39:48 +0900

    Middleware Recap

--- src/server.js
+++ src/server.js
@@ -4,11 +4,26 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
+const routerLogger = (req, res, next) => {
+  console.log("PATH", req.path);
+  next();
+};
+const methodLogger = (req, res, next) => {
+  console.log("METHOD", req.method);
+  next();
+};
+const home = (req, res) => {
+  console.log("I will respond");
   return res.send("<h1>Hello!</h1>");
 };
 
-app.get("/", handleHome);
+const login = (req, res) => {
+  return res.send("login here");
+};
+
+app.use(routerLogger, methodLogger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Sever listening on port http://localhost:${PORT}🚀`);
