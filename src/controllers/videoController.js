e56da7ae861c2b840643c609a2fc6bff02954b7a
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-11-01 19:34:05 +0900

    Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,27 @@
 export const trending = (req, res) => {
-  const videos = [];
+  const videos = [
+    {
+      title: "First Video",
+      rating: "3",
+      comments: "2",
+      createdAt: "5",
+      views: "223",
+    },
+    {
+      title: "Second Video",
+      rating: "2",
+      comments: "22",
+      createdAt: "11",
+      views: "233",
+    },
+    {
+      title: "Third Video",
+      rating: "1",
+      comments: "211",
+      createdAt: "8",
+      views: "44.231",
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => res.render("watch");
