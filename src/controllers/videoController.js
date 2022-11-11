1f2150af93a83c085e3c452e75254ec2cb37bafa
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-11-11 16:06:48 +0900

    Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -57,9 +57,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -74,7 +72,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
