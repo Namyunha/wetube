9263fbb40c281ed0680c0fe20b5a5aba744e9e7c
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-11-18 13:54:08 +0900

    Edit Profile POST part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -141,8 +141,20 @@ export const logout = (req, res) => {
 export const getEdit = (req, res) => {
   res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
-export const postEdit = (req, res) => {
-  res.redirect("/");
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, password },
+  } = req;
+  await User.findByIdAndUpdate(_id, {
+    name,
+    email,
+    username,
+    location,
+  });
+  return res.render("edit-profile");
 };
 
 export const see = (req, res) => res.send("See User");
