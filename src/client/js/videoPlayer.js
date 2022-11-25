19c5943951cf7dbb6565a96106e0fece5fa371c1
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-11-25 17:37:58 +0900

    Mute and Unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,26 +2,26 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volumeRange");
 
 const handlePlay = (e) => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
-  } // if the video is playing, pause it
-  // else play the video
-};
-const handlePause = () => {
-  playBtn.innerText = "Play";
-};
-const handlePlayClick = () => {
-  playBtn.innerText = "Pause";
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener("click", handlePlay);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlayClick);
