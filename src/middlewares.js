840a7500df51de334b78d8fcfe4ad92a5addde67
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-12-03 23:03:27 +0900

    Remove Avatar

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,4 @@
-import multer from "multer";
+// import multer from "multer";
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -25,12 +25,12 @@ export const publicOnlyMiddleware = (req, res, next) => {
   }
 };
 
-export const avatarUpload = multer({
-  dest: "uploads/avatars/",
-  limits: {
-    fileSize: 3000000,
-  },
-});
+// export const avatarUpload = multer({
+//   dest: "uploads/avatars/",
+//   limits: {
+//     fileSize: 3000000,
+//   },
+// });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
