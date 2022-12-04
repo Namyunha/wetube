a83a33ec18d03142107ef9f4b2968723a164bd8e
Namyunha <108773845+Namyunha@users.noreply.github.com>
2022-12-04 21:32:26 +0900

    Static Files(updated)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -117,7 +117,7 @@ export const finishGithubLogin = async (req, res) => {
     let user = await User.findOne({ email: emailObj.email });
     if (!user) {
       user = await User.create({
-        // avatarUrl: userData.avatar_url,
+        avatarUrl: userData.avatar_url,
         name: userData.name ? userData.name : userData.login,
         username: userData.login,
         email: emailObj.email,
@@ -146,17 +146,15 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
-      // user: { _id, avatarUrl },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
   } = req;
-  console.log(file);
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      // avatarUrl: file ? file.path : avatarUrl,
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
